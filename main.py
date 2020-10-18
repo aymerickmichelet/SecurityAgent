d4250110e96959de0c6496651f765d5403ea8dc0
Aymerick Michelet <contact@aymerickmichelet.fr>
2020-10-19 01:28:34 +0200

    edit role

--- main.py
+++ main.py
@@ -16,7 +16,6 @@
 import asyncio
 import discord
 from discord.ext import commands
-from discord.utils import get
 
 client = commands.Bot(command_prefix="!")
 tocken = "NzU5ODc2NTU5Nzc5MTM1NTI5.X3D4MQ.vNF_4CtAIKqb16yih5AxA4uzy_8"
@@ -30,18 +29,27 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     print(str(member) + " vient de rejoindre le serveur.")
-
-    # print(member.guild)
-    # print(client.guilds)
-
-    await formWelcome(member)
+    await form_welcome(member)
 
 @client.event
 async def on_member_remove(member):
     print(str(member) + " vient de quitter le serveur.")
 
 
-async def formWelcome(member):
+def info_member(member, firstname, lastname, school, level, delegate):
+    embed = discord.Embed(color=0x000000)
+    embed.set_author(name=str(member), icon_url=member.avatar_url)
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/icons/619567457123958813/3d33b155ee4c990c7d89b394fee4bbdd.webp?size=128")
+    embed.add_field(name="prénom:", value=firstname, inline=True)
+    embed.add_field(name="nom:", value=lastname, inline=True)
+    embed.add_field(name="école:", value=school, inline=False)
+    embed.add_field(name="level:", value=level, inline=True)
+    embed.add_field(name="delegate:", value=delegate, inline=True)
+    embed.set_footer(text="(Agent de Sécurité) - Bot by Aymerick MICHELET")
+    return embed
+
+async def form_welcome(member):
 
     global client
 
@@ -56,72 +64,89 @@ async def formWelcome(member):
     text6 = "... ton école ? \n*réponses acceptées: `EPSI`, `WIS`*"
     text7 = "Dans quelle promotion rentres-tu ? \n*réponses acceptées: `B1`, `B2`, `B3`, `I1`, `I2`*"
     text8 = "Pour finir, es-tu délégué de ta promotion ?\n*réponses acceptées: `Oui`, `Non`*"
-    
-    questions = [text4, text5, text6, text7, text8]
+    text9 = "Ces informations sont correctes ?\n*réponses acceptées: `Oui`, `Non`*"
+    text10 = "Bon, je vais recommencer le questionnaire..."
+    text11 = "Bienvenue sur le discord !"
+
+    questions = [text4, text5, text6, text7, text8, text9]
     values = ["", "", "", "", False]
 
-    await member.send(content=text1+"\n"+text2+"\n"+text3)
+    await member.send(content=text1 + "\n" + text2 + "\n" + text3)
 
     await asyncio.sleep(5)
-    
-    def check(message):
-        return message.author == member and message.channel == member.dm_channel
 
-    for i in range(0, len(questions)):
-        count_limit = 10
-        count = 0
-        await member.send(content=questions[i])
-        while True:
-            try:
-                values[i] = await client.wait_for("message", timeout = 30, check = check)
-            except:
-                count+=1
-                if count < count_limit:
-                    await member.send(content=text_alert)
-                    continue
-                else:
-                    await member.send(content=text_timeout)
-                    await member.kick(reason="No response during the welcome form")
-                    return
-            msg = values[i].content.upper()
-            values[i] = values[i].content
-            if i == 2:
-                result = msg == "EPSI" or msg == "WIS"
-            elif i == 3:
-                result = msg == "B1" or msg == "B2" or msg == "B3" or msg == "I1" or msg == "I2"
-            elif i == 4:
-                result = msg == "OUI" or msg == "NON"
+    while True:
+
+        def check(message):
+            return message.author == member and message.channel == member.dm_channel
+
+        for i in range(0, len(questions)):
+            count_limit = 10
+            count = 0
+            await member.send(content=questions[i])
+            if i != 5:
+                while True:
+                    try:
+                        values[i] = await client.wait_for("message", timeout=30, check=check)
+                    except:
+                        count+=1
+                        if count < count_limit:
+                            await member.send(content=text_alert)
+                            continue
+                        else:
+                            await member.send(content=text_timeout)
+                            await member.kick(reason="No response during the welcome form")
+                            return
+                    msg = values[i].content.upper()
+                    values[i] = values[i].content
+                    if i == 2:
+                        result = msg == "EPSI" or msg == "WIS"
+                    elif i == 3:
+                        result = msg == "B1" or msg == "B2" or msg == "B3" or msg == "I1" or msg == "I2"
+                    elif i == 4:
+                        result = msg == "OUI" or msg == "NON"
+                    else:
+                        result = True
+                    if result:
+                        count = 0
+                        break
+                    else:
+                        await member.send(content=text_wrong_answer)
             else:
-                result = True
-            if result:
-                count = 0
-                break
-            else:
-                await member.send(content=text_wrong_answer)
-
-    embed=discord.Embed(color=0x000000)
-    embed.set_author(name=str(member), icon_url=member.avatar_url)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/icons/619567457123958813/3d33b155ee4c990c7d89b394fee4bbdd.webp?size=128")
-    embed.add_field(name="prénom:", value=values[0], inline=True)
-    embed.add_field(name="nom:", value=values[1], inline=True)
-    embed.add_field(name="école:", value=values[2], inline=False)
-    embed.add_field(name="level:", value=values[3], inline=True)
-    embed.add_field(name="delegate:", value=values[4], inline=True)
-    embed.set_footer(text="(Agent de Sécurité) - Bot by Aymerick MICHELET")
-    await member.send(embed=embed)
-
-    # role = discord.utils.get(member.server.roles, name=values[2].upper()+"-"+values[3].upper())
-
-    role = get(member.guild.roles, name="EPSI-B3")
-    aymnms = member.guild.get_member(member.guild.owner_id)
-
-    print(role) # role
-    print(aymnms) # member
-
-    await member.edit(nick=values[0].lower()+"."+values[1].lower())
-    await aymnms.add_roles(role)
+                await member.send(embed=info_member(member, values[0], values[1], values[2], values[3], values[4]))
+                while True:
+                    try:
+                        msg = await client.wait_for("message", timeout=30, check=check)
+                    except:
+                        count += 1
+                        if count < count_limit:
+                            await member.send(content=text_alert)
+                            continue
+                        else:
+                            await member.send(content=text_timeout)
+                            await member.kick(reason="No response during the welcome form")
+                            return
+                    msg = msg.content.upper()
+                    if msg == "OUI":
+                        role = discord.utils.get(member.guild.roles, name=values[2].upper() + "-" + values[3].upper())
+                        await member.edit(nick=values[0].lower() + "." + values[1].lower())
+                        await member.edit(roles=[role])
+                        await member.send(content=text11)
+                        return
+                    elif msg == "NON":
+                        await member.send(content=text10)
+                        break
+                    else:
+                        result = False
+                    if result:
+                        count = 0
+                        break
+                    else:
+                        await member.send(content=text_wrong_answer)
 
 client.run(tocken)
 
 # gestion user leave serveur pendant discussion mp
-
+# faire message channel Accueil de bienvenue à la fin du questionnaire
+# gestion des questions / text dans fichiers config
+# gestion logs (channel logs)
